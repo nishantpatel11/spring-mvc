9d8fd4815226935abf28a89da4fc215224050faf
nishant patel <nishantpatelbca@gmail.comgit config --global user.name nishantpatel11>
2019-11-08 13:56:02 +0530

    Added New Project

---
+++ src/main/java/com/spring/demo/mvc/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.spring.demo.mvc.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+	 
+	    @GetMapping("/")
+	    public String userForm(Locale locale, Model model) {
+	        model.addAttribute("users", "Hello");
+	        return "editUsers";
+	    }
+	    
+	    
+	    @GetMapping("/user")
+//	    @ResponseBody
+	    public String user(Locale locale, Model model) {
+	        model.addAttribute("users", "Hello");
+	        return "editUsers";
+	    }
+	    
+	    
+	    @RequestMapping(value = "/users/{name}", method = RequestMethod.GET)
+		public ModelAndView hello(@PathVariable("name") String name) {
+
+			ModelAndView model = new ModelAndView();
+			model.setViewName("hello");
+			model.addObject("msg", name);
+
+			return model;
+
+		}
+}
